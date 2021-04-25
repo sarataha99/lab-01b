968332e4183f461ea1342ed1972227fca2adadd7
sarataha99 <sara.taha2020@outlook.com>
2021-04-25 15:26:17 +0300

    sara

---
+++ app.js
@@ -0,0 +1,18 @@
+let userName=prompt('what is Your favorite book type?')
+console.log( '  Your favorite book type is  ' +  userName);
+alert('It Is Amazing!');
+
+let author=prompt(' what is your favorite author? ' )
+console.log(  ' your favorite author is ' + author);
+alert('it is good author and I like him');
+
+let language=prompt('What language would you like to read books?')
+console.log(   ' language would you like to read books is '  + language)
+
+let yourname=prompt('what is your Name?');
+console.log(' your Name is  '  + yourname);
+
+let age=prompt('what is your age?');
+console.log( ' your age is ' + age);
+
+alert( ' Hello '  +  ' your Name is ' +  yourname + ' your age is '  +  age  + ' your favorite book is '  +  userName +  ' your favorite author is '   +  author  + ' language would you like to read books is '  +   language )
